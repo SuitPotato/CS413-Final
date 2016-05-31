8daf969b79c76e5cdb638a7cde2d6cffff8cecd7
スーツ芋 <Erekyth@yahoo.com>
2016-05-31 15:34:13 -0700

    Arrow Tower Find Target

--- CS413 Final/towerUnits.js
+++ CS413 Final/towerUnits.js
@@ -20,6 +20,31 @@ function arrowTowerSetup(x,y){
 	arrowTower.target = null;
 }
 
+// Lets the arrow tower find a target
+arrowTower.findTarget = function() {
+	// If there are no enemies, then there is no target
+	if(enemies.length === 0) {
+		arrowTower.target = null;
+		return;
+	}
+	
+	// If the target is defeated, then remove target
+	if(arrowTower.target && arrowTower.target.ife <= 0) {
+		arrowTower.target = null;
+	}
+	
+	// Find the first enemy within the range and target
+	for(var i = 0, j = enemies.length; i < j; i++){
+		// Towerdimension is added to look at the center of the square.
+		var dist = (enemies[i].x-arrowTower.x)*(enemies[i].x-arrowTower.x+TOWERDIMENSION)+
+			(enemies[i].y-arrowTower.y)*(enemies[i].y-arrowTower.y+TOWERDIMENSION);
+		if(dist < (arrowTower.range * arrowTower.range)) {
+			arrowTower.target = enemies[i];
+			return;
+		}
+	}
+}
+
 
 
 
